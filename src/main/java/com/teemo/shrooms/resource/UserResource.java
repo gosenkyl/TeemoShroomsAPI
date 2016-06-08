bdc44b7679963cac087fe630666e1fb1b63fd511
cah-kylegosen <kyle.gosen01@cardinalhealth.com>
2016-06-08 16:55:04 -0400

    Fixed User Resource

--- src/main/java/com/teemo/shrooms/resource/UserResource.java
+++ src/main/java/com/teemo/shrooms/resource/UserResource.java
@@ -6,51 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping(value = "api/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin
 public class UserResource {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public List<User> getAllUsers(){
         return userService.findAll();
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public User getUserById(@PathVariable String id){
         return userService.findById(id);
     }
 
-    @RequestMapping(value = "user/{userName}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(value = "user/{userName}", method = RequestMethod.GET)
     public List<User> getUserByUserName(@PathVariable String userName){
         return userService.getUserByUserName(userName);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public User createOrUpdateEntity(User entity) {
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public User createOrUpdateEntity(@RequestBody User entity) {
         entity.setLastPolled(new Date());
 
         return userService.save(entity);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<User> createOrUpdateEntities(List<User> entities) {
-        for(User user : entities){
-            user.setLastPolled(new Date());
-        }
-
-        return userService.save(entities);
-    }
-
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public void deleteEntity(@PathParam("id") String id) {
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void deleteEntity(@PathVariable String id) {
         userService.delete(id);
     }
 
